9e654677cf5dfd77d1d45a8c5fa97030d259a3e1
misael <misael.reynoso.aguayo@gmail.com>
2026-03-27 13:20:31 -0600

    feat: inicializar app Kitchy y configurar CORS para desarrollo

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Inicializamos la aplicación FastAPI. 
+# Esto genera automáticamente la documentación Swagger que luego usaremos para Dart.
+app = FastAPI(
+    title="Kitchy API",
+    description="Backend",
+    version="1.0.0"
+)
 
+# CORS
+# Definimos quién tiene permiso de comunicarse con nuestra API.
+# En desarrollo usamos ["*"] para permitir que el emulador
+# de Flutter se conecte sin problemas. En producción (cuando lo subas a un servidor real)
+# cambiaremos esto estrictamente por el dominio de tu app
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite peticiones de cualquier origen (solo para desarrollo local)
+    allow_credentials=True, # Permite el envío de cookies y credenciales de autenticación
+    allow_methods=["*"],  # Permite todos los métodos HTTP (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Permite todos los encabezados
+)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Creamos nuestro primer Endpoint (Ruta) de prueba
+@app.get("/", tags=["Health Check"])
+def health_check():
+    """
+    Ruta raíz para verificar que el servidor está levantado y funcionando.
+    """
+    return {
+        "estado": "OK",
+        "mensaje": "¡El motor de Kitchy está funcionando correctamente!"
+    }
