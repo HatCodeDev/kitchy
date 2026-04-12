9cc91b8cdb956e304d3d606927b5a3ccf50e152f
misael <misael.reynoso.aguayo@gmail.com>
2026-04-12 13:11:52 -0600

    feat: E5-05 Modelo SQLAlchemy MovimientoInsumo (Inmutable)

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,4 @@
-# app/models/__init__.py
 from app.core.database import Base
 from app.models.user import User
 from app.models.insumo import Insumo
+from app.models.movimiento_insumo import MovimientoInsumo
